650a06d830d0859b6e80ae731190cc9a80efa39d
Tensor <narutokelechi@gmail.com>
2024-03-17 21:17:09 +0100

    Version 2. Update UI

--- text_lens.py
+++ text_lens.py
@@ -6,35 +6,52 @@ load_dotenv()
 
 st.set_page_config(page_title="TextLens")
 
-st.title("Image classification (Crops)")
+st.title("Image text extraction(OCR model)")
 st.header("Model UI")
 
+# file_tab, camera_tab = st.tabs(["Files", "Camera"])
 
+# with file_tab:
 image_upload = st.file_uploader(
-    "Please upload an image(Face)", type=["png", "jpg", "jpeg"]
-)
+        "Please upload an image(with text)", type=["png", "jpg", "jpeg"]
+    )
 
-text_extract = replicate.run(
-    "abiruyt/text-extract-ocr:a524caeaa23495bc9edc805ab08ab5fe943afd3febed884a4f3747aa32e9cd61",
-    input={
-        "image": image_upload,
-    },
-)
+# with camera_tab:
+#     camera_upload = st.camera_input("Take pic with phone")
 
+
+def extract_text():
+    text_extract = replicate.run(
+        "abiruyt/text-extract-ocr:a524caeaa23495bc9edc805ab08ab5fe943afd3febed884a4f3747aa32e9cd61",
+        input={
+            "image": image_upload,
+        },
+    )
+    # text_extract = st.write_stream(text_extract)
+    return text_extract
+
+# def extract_input():
+#     if camera_tab:
+#         extract_text(camera_upload)
+#     else:
+#         extract_text(image_upload)
+        
 
 if image_upload:
     if st.button("Extract text"):
-        with st.spinner("Extracting..."):
+        image_extract = extract_text()
+        
+        with st.chat_message('user'):
+            st.write("This is the extracted text")
             st.markdown(
                 f"""
-                    <div style="background-color: black; color: white; font-weight: bold; padding: 1rem; border-radius: 10px;">
-                    <h4>Image text</h4>
-                    {text_extract}
+                    <div style="background-color: black; color: white; font-weight: bold; padding: 1rem; margin-bottom: 0.5rem; border-radius: 10px;">
+                    {image_extract}
                 """,
                 unsafe_allow_html=True,
             )
+            
     st.success("Successful")
-
 
 
 # print(text_extract)
@@ -55,8 +72,8 @@ st.markdown(
     </div>
     
     <div style="text-align: center; padding: 1rem;">
-        Data from <a href="https://kaggle.com" target="_blank" style="color: lightblue; font-weight: bold; text-decoration: none;">
-         Kaggle</a>
+        Powered by <a href="https://replicate.com" target="_blank" style="color: lightblue; font-weight: bold; text-decoration: none;">
+         ReplicateAI models</a>
     </div>
 """,
     unsafe_allow_html=True,
